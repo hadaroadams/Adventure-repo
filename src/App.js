841e86447e63a6a3f51e260430c97d94ede6b665
hadaroadams <hadaroadams1234@gmail.com>
2022-12-26 14:23:32 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
